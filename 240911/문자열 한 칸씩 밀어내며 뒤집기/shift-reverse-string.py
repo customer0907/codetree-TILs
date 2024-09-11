78b0a2502fe783502c633f594592e588db3b12d7
LEECHANGHEE <129058247+customer0907@users.noreply.github.com>
2024-09-11 15:37:13 +0900

    Title: 문자열 한 칸씩 밀어내며 뒤집기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240911/문자열 한 칸씩 밀어내며 뒤집기/shift-reverse-string.py
@@ -0,0 +1,21 @@
+import sys
+
+word, n = sys.stdin.readline().split()
+n = int(n)
+
+arr = []
+
+for i in range(n):
+    arr.append(sys.stdin.readline().strip())
+
+for idx in arr:
+    if(idx=='1'):
+        word = word[1:]+word[0]
+        print(word)
+    elif(idx=='2'):
+        word = word[-1]+word[0:-1]
+        print(word)
+    else:
+        word = list(word)
+        word.reverse()
+        print(''.join(word))
