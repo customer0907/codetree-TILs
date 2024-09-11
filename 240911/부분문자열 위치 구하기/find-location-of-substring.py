86636efa3dc9649e04b5f47e64557a78cb2c697a
LEECHANGHEE <129058247+customer0907@users.noreply.github.com>
2024-09-11 11:55:43 +0900

    Title: 부분문자열 위치 구하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240911/부분문자열 위치 구하기/find-location-of-substring.py
+++ 240911/부분문자열 위치 구하기/find-location-of-substring.py
@@ -5,12 +5,12 @@ target = input()
 is_matched = False
 
 for i in range(len(inp)):
+    
+    is_matched = True
     for j in range(len(target)):
-        if(inp[i+j] == target[j]):
-            is_matched = True
-        else:
+        if(inp[i+j] != target[j]):
             is_matched = False
-            break
+
     if(is_matched == True):
         print(i)
         sys.exit(0)
