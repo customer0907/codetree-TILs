35f187523e75c75cbd7791c9b68bfbced8ac077d
LEECHANGHEE <129058247+customer0907@users.noreply.github.com>
2024-09-11 17:36:24 +0900

    Title: 사칙연산 함수, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240911/사칙연산 함수/quadratic-operations-function.py
@@ -0,0 +1,19 @@
+import sys
+
+a,b,c = sys.stdin.readline().split()
+a=int(a)
+c=int(c)
+
+def fourCal(fst_num, opt, snd_num):
+    if(opt == '+'):
+        print(f"{fst_num} + {snd_num} = {fst_num+snd_num}")
+    elif(opt == '-'):
+        print(f"{fst_num} - {snd_num} = {fst_num-snd_num}")
+    elif(opt == '/'):
+        print(f"{fst_num} / {snd_num} = {fst_num/snd_num}")
+    elif(opt == '*'):
+        print(f"{fst_num} * {snd_num} = {fst_num*snd_num}")
+    else:
+        print("False")
+
+fourCal(a,b,c)
