d694146bd61c7ebc600e63344db23fe2fb786b68
LEECHANGHEE <129058247+customer0907@users.noreply.github.com>
2024-09-11 18:27:51 +0900

    Title: 그 계절, 그 날, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240911/그 계절, 그 날/that-season-that-day.py
@@ -0,0 +1,41 @@
+import sys
+
+y,m,d = [int(x) for x in sys.stdin.readline().split()]
+
+def leap_year(year):
+    is_leap = False
+    if(year%4 ==0):
+        is_leap = True
+        if(year%100 ==0):
+            is_leap = False
+            if(year%400 == 0):
+                is_leap = True
+    else:
+        is_leap = False
+    return is_leap
+
+def print_season(month):
+    if(month >=3 and month <=5):
+        print("Spring")
+    elif(month >=6 and month <=8):
+        print("Summer")
+    elif(month >=9 and month <=11):
+        print("Fall")
+    else:
+        print("Winter")
+
+if(m == 2 or m == 4 or m == 6 or m == 9 or m == 11):
+    if(m==2 and d > 28):
+        if leap_year(y): # 윤년인 경우
+            print_season(m)
+            sys.exit(0)
+        else: # 윤년이 아닌 경우
+            print("-1")
+            sys.exit(0)
+    if(d==31):
+        print("-1")
+    else:
+        print_season(m)
+        sys.exit(0)
+else:
+    print_season(m)
