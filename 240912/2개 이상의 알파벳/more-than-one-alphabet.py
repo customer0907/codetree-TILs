9d54d0d91b055f24d673b87688c8cabb6fe45ed4
LEECHANGHEE <129058247+customer0907@users.noreply.github.com>
2024-09-12 13:22:15 +0900

    Title: 2개 이상의 알파벳, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240912/2개 이상의 알파벳/more-than-one-alphabet.py
+++ 240912/2개 이상의 알파벳/more-than-one-alphabet.py
@@ -2,9 +2,11 @@ import sys
 
 word = list(sys.stdin.readline())
 
-after_word = set(word)
+def alphaOverTwo(_word):
+    after_word = set(_word)
+    if(len(after_word)>=2):
+        print("Yes")
+    else:
+        print("No")
 
-if(len(after_word)>=2):
-    print("Yes")
-else:
-    print("No")
+alphaOverTwo(word)
