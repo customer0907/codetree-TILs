449b9a2d31d9b5d41ffc90f15cf7fd1d996a266d
LEECHANGHEE <129058247+customer0907@users.noreply.github.com>
2024-10-01 17:48:48 +0900

    Title: Date to Date, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 241001/Date to Date/date-to-date.py
+++ 241001/Date to Date/date-to-date.py
@@ -2,12 +2,12 @@ import sys
 
 m1, d1, m2, d2 = map(int,sys.stdin.readline().split())
 
-monthDays = [31,28,31,30,31,30,31,31,30,31,30,31]
+monthDays = [0,31,28,31,30,31,30,31,31,30,31,30,31] # size : 13
 
 afterSum = 0
 beforeSum = 0
 
-for i in range(1,m2):
+for i in range(1,m2): # 12월이면, 11인덱스까지.
     afterSum += monthDays[i]
 
 for i in range(1,m1):
