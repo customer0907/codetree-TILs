3d98475ebc04dc192706985ee61ad578685d3ffa
LEECHANGHEE <129058247+customer0907@users.noreply.github.com>
2024-10-01 17:40:10 +0900

    Title: Date to Date, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241001/Date to Date/date-to-date.py
+++ 241001/Date to Date/date-to-date.py
@@ -7,14 +7,10 @@ monthDays = [31,28,31,30,31,30,31,31,30,31,30,31]
 afterSum = 0
 beforeSum = 0
 
-for i in range(m2):
+for i in range(1,m2):
     afterSum += monthDays[i]
 
-for i in range(m1):
+for i in range(1,m1):
     beforeSum += monthDays[i]
 
-if(m1==m2):
-    if(d1==d2):
-        print(1)
-    else:
-        print(afterSum+d2-(beforeSum+d1))
+print((afterSum+d2)-(beforeSum+d1))
