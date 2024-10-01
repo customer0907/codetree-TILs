f135679517b10930f39330e27865d1c38ed24e16
LEECHANGHEE <129058247+customer0907@users.noreply.github.com>
2024-10-01 17:32:37 +0900

    Title: Date to Date, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241001/Date to Date/date-to-date.py
@@ -0,0 +1,16 @@
+import sys
+
+m1, d1, m2, d2 = map(int,sys.stdin.readline().split())
+
+monthDays = [31,28,31,30,31,30,31,31,30,31,30,31]
+
+afterSum = 0
+beforeSum = 0
+
+for i in range(m2):
+    afterSum += monthDays[i]
+
+for i in range(m1):
+    beforeSum += monthDays[i]
+
+print(afterSum+d2-(beforeSum+d1))
