d55a9eb15dba6fa4e2a36914f0c27033d407e8ab
LEECHANGHEE <129058247+customer0907@users.noreply.github.com>
2024-10-07 17:25:01 +0900

    Title: 최대로 겹치는 지점, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241007/최대로 겹치는 지점/maximum-overlapped-points.py
+++ 241007/최대로 겹치는 지점/maximum-overlapped-points.py
@@ -6,7 +6,7 @@ arr = []
 is_overlap = [0] * 100
 
 for i in range(N):
-    x1, x2 = map(int,sys.stdin.readline().split())
+    x1, x2 = map(int,input().split())
     for j in range(x1,x2+1):
         is_overlap[j] += 1
 
