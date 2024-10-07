8d31b92bc55b2814570922ef9ddf315f71e85a0e
LEECHANGHEE <129058247+customer0907@users.noreply.github.com>
2024-10-07 17:30:40 +0900

    Title: 최대로 겹치는 지점, Time: 122ms, Memory: 25MB, Status: Passed - CodeTree

--- 241007/최대로 겹치는 지점/maximum-overlapped-points.py
+++ 241007/최대로 겹치는 지점/maximum-overlapped-points.py
@@ -1,17 +1,16 @@
 import sys
 
-N = sys.stdin.readline().split()
-N = int(N)
+N = int(sys.stdin.readline())
 
 arr = []
-is_overlap = [0] * 100
+is_overlap = [0] * 100001  # 최대 범위를 고려하여 크기 조정
 
-for i in range(N):
-    x1, x2 = map(int,sys.stdin.readline().split())
-    arr.append((x1,x2))
+for _ in range(N):
+    x1, x2 = map(int, sys.stdin.readline().split())
+    arr.append((x1, x2))
 
-for x1,x2 in arr:
-    for j in range(x1,x2+1):
-        is_overlap[j] +=1
-        
+for x1, x2 in arr:
+    for j in range(x1, x2 + 1):
+        is_overlap[j] += 1
+
 print(max(is_overlap))
